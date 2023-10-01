c261570d38ac98980e40c374c3b210efa3dd23d2
bsf13 <143499390+bsf13@users.noreply.github.com>
2023-10-01 16:45:51 +0300

    Update create_track_number_test.py

--- create_track_number_test.py
+++ create_track_number_test.py
@@ -6,11 +6,9 @@ import data
 def test_create_order_by_track():
     response_3 = sender_stand_request.post_new_order(data.order_body)
     track = {"t": response_3.json()["track"]}
-    print("Создан заказ, номер трека заказа:", track)
-
+   
 # Получение сведений о заказе по номеру трека
     response_4 = sender_stand_request.get_new_order(track)
     assert response_4.status_code == 200
     order_info = response_4.json()
-    print("Сведения о заказе:", order_info)
-
+ 
